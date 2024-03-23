c86fcf488120a2d500708213d164a479111cfd88
Emmanuel Guefif <eguefif@gmail.com>
2024-03-23 09:12:19 -0400

    setup basic

--- module03/lib/widget/top_bar.dart
+++ module03/lib/widget/top_bar.dart
@@ -3,7 +3,8 @@ import 'package:module03/controllers/geodata_fetcher.dart';
 import 'package:module03/models/default_search_values.dart';
 
 class TopBar extends StatefulWidget {
-  const TopBar({super.key, required this.changeLocation, required this.switchLoading});
+  const TopBar(
+      {super.key, required this.changeLocation, required this.switchLoading});
 
   final void Function(List<double>) changeLocation;
   final void Function() switchLoading;
@@ -16,88 +17,78 @@ class TopBar extends StatefulWidget {
 
 class _TopBar extends State<TopBar> {
   List<Map<String, dynamic>> entries = defaultValues;
-  SearchController? _controller;
   bool hasTapped = false;
+  late TextEditingController controller;
 
-  void refreshView() {
-    if (_controller != null) {
-      SearchController controller = _controller!;
-      if (controller.text.length >= 3) {
-        geoDataFetcher(controller.text).then(
-          (fetcherRetval) {
-            setState(
-              () {
-                entries = fetcherRetval;
-                //controller.openView();
-              },
-            );
-          },
-        ).catchError((error) {
-          entries = defaultValues;
-        });
-      } else {
-        entries = defaultValues;
-      }
-    }
+  @override
+  void initState(){
+    super.initState();
+    controller = TextEditingController();
   }
 
   @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  void updateView(String value) {
+    print("Update $value");
+  }
+
+  void refreshView() {
+    geoDataFetcher("paris").then(
+      (fetcherRetval) {
+        setState(
+          () {
+            entries = fetcherRetval;
+            //controller.openView();
+          },
+        );
+      },
+    ).catchError((error) {
+      entries = defaultValues;
+    });
+  }
+
+  void selectItem() {}
+
+  @override
   Widget build(BuildContext context) {
-    return SearchAnchor(
-      builder: (context, controller) => SearchBar(
-        controller: controller,
-        padding: const MaterialStatePropertyAll(EdgeInsets.all(3.0)),
-        onTap: () {
-          controller.addListener(refreshView);
-          _controller = controller;
-          controller.openView();
-        },
-        onChanged: (_) {
-          controller.openView();
-        },
-        onSubmitted: (_) {
-          controller.removeListener(refreshView);
-          controller.closeView(controller.text);
-        },
-        leading: const Icon(Icons.search),
-      ),
+    return TextField(
+      controller: controller,
+      onChanged: (String value) async {updateView(value);},
+    );
+  }
+}
+/*
       suggestionsBuilder: (context, controller) => List<Column>.generate(
         5,
         (int index) {
-          final String city = entries[index]["city"];
-          final String state = entries[index]["state"];
-          final String country = entries[index]["country"];
-
-          String entry = city;
-          if (state != "None") {
-            entry += " $state";
-          }
-          entry += " $country";
-          return Column(
-            children: [
-              ListTile(
-                title: Text(entry),
-                leading: const Icon(Icons.location_city),
-                onTap: () {
-                  setState(
-                    () {
-                      hasTapped = true;
-                      widget.switchLoading();
-                      controller.closeView(entry);
-                      controller.removeListener(refreshView);
-                      widget.changeLocation([
-                        entries[index]["latitude"],
-                        entries[index]["longitude"],
-                      ]);
-                    },
-                  );
-                },
-              ),
-              if (index < 4) const Divider(),
-            ],
-          );
+          return getCityItem(entries[index], selectItem, index);
         },
-      ),
-    );
+        */
+
+Column getCityItem(
+    Map<String, dynamic> data, void Function() selectItem, int index) {
+  final String city = data["city"];
+  final String state = data["state"];
+  final String country = data["country"];
+
+  String entry = city;
+  if (state != "None") {
+    entry += " $state";
   }
+  entry += " $country";
+  return Column(
+    children: [
+      ListTile(
+          title: Text(entry),
+          leading: const Icon(Icons.location_city),
+          onTap: () {
+            selectItem();
+          }),
+      if (index < 4) const Divider(),
+    ],
+  );
 }
